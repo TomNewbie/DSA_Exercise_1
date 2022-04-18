f6226a3efb626eee4e3c74bdb515f27d27359866
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-04-18 14:03:38 +0700

    add hash map

---
+++ Exercise_HashTable/chaining.cpp
@@ -0,0 +1,93 @@
+#include <cmath>
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+
+struct phoneData {
+    string name;
+    string phoneNumber;
+    phoneData(string name, string phoneNumber) : name{name}, phoneNumber{phoneNumber} {}
+};
+template <int N>
+class HashMap {
+private:
+    // phoneData data;
+    list<phoneData *> phoneBook[N];
+    int Hash(string name)
+    {
+        int sum = 0;
+        for (int i = 0; i < name.length(); i++) {
+            sum += (int)name[i] % N;
+        }
+        return sum % N;
+    }
+
+public:
+    void Insert(string key, string phoneNumber)
+    {
+        phoneData *data = new phoneData(key, phoneNumber);
+        int hashData = Hash(key);
+        phoneBook[hashData].push_front(data);
+    }
+    void Search(string key)
+    {
+        int hashData = Hash(key);
+        for (auto data : phoneBook[hashData]) {
+            if (data->name == key) {
+                cout << "Data found: " << endl;
+                cout << "Name: " << data->name << "  Phone number: " << data->phoneNumber << endl;
+                return;
+            }
+        }
+        cout << "Data "<< key << " not found!" << endl;
+    }
+    void DeleteKey(string key)
+    {
+        int hashData = Hash(key);
+        for (auto data : phoneBook[hashData]) {
+            if (data->name == key) {
+                phoneBook[hashData].remove(data);
+                cout << "Delete data " << key << " succesfully!" << endl;
+                return;
+            }
+        }
+        cout << "Data not found to delete!" << endl;
+    }
+    void Display()
+    {
+        for(int i = 0; i < N; i++)
+        {
+            cout << "bucket " << i << " ---> ";
+            for(auto data: phoneBook[i])
+            {
+                cout << data->name << ": " << data->phoneNumber;
+                if(data != phoneBook[i].back())
+                {
+                    cout << " ---> ";
+                }
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    HashMap<10> thobook;
+    thobook.Insert("Tho", "090312231");
+    thobook.Insert("thanh ga", "021321312");
+    thobook.Insert("Quan gar", "21321321");
+    thobook.Insert("Somi", "21321321");
+    thobook.Insert("Nam", "69696321");
+    thobook.Insert("Nghi", "1234521");
+
+    thobook.Search("thanh ga");
+    thobook.Search("th");
+    thobook.Display();
+    thobook.DeleteKey("Somi");
+    thobook.Display();
+
+    cout << "finish";
+    return 0;
+}
---
+++ Exercise_HashTable/linearProbing.cpp
@@ -0,0 +1,120 @@
+#include <cmath>
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+
+struct phoneData {
+    string name;
+    string phoneNumber;
+    phoneData(string name, string phoneNumber) : name{name}, phoneNumber{phoneNumber} {}
+};
+template <int N>
+class HashMap {
+private:
+    // phoneData data;
+    phoneData *phoneBook[N];
+    int Hash(string name)
+    {
+        int sum = 0;
+        for (int i = 0; i < name.length(); i++) {
+            sum += (int)name[i] % N;
+        }
+        return sum % N;
+    }
+
+public:
+    HashMap()
+    {
+        for (int i = 0; i < N; i++) {
+            phoneBook[i] = nullptr;
+        }
+    }
+    void Insert(string key, string phoneNumber)
+    {
+        phoneData *data = new phoneData(key, phoneNumber);
+        int hashData = Hash(key);
+        int i = 0;
+        while (i < N) {
+            i++;
+            if (phoneBook[hashData] == nullptr) {
+                phoneBook[hashData] = data;
+                return;
+            }
+            else {
+                hashData = (hashData + 1) % N;
+            }
+        }
+        cout << "Data is full please allocate more memory!" << endl;
+    }
+    void Search(string key)
+    {
+        int hashData = Hash(key);
+        int i = 0;
+        while (i < N) {
+            i++;
+            if(phoneBook[hashData] == nullptr) break;
+            if (phoneBook[hashData]->name == key) {
+                cout << "Data found: " << endl;
+                cout << "Name: " << phoneBook[hashData]->name << "  Phone number: " << phoneBook[hashData]->phoneNumber << endl;
+                return;
+            }
+            else {
+                hashData = (hashData + 1) % N;
+            }
+        }
+        cout << "Data " << key << " not found!" << endl;
+    }
+    void DeleteKey(string key)
+    {
+        int hashData = Hash(key);
+        int i = 0;
+        while (i < N) {
+            i++;
+            if(phoneBook[hashData] == nullptr) break;
+            if (phoneBook[hashData]->name == key) {
+                phoneBook[hashData] = nullptr;
+                cout << "Data deleted successfully!" << endl;
+                return;
+            }
+            else {
+                hashData = (hashData + 1) % N;
+            }
+        }
+        cout << "Data not found to delete!" << endl;
+    }
+    void Display()
+    {
+        for (int i = 0; i < N; i++) {
+            cout << "Data " << i << ": ";
+            if(phoneBook[i]==nullptr){
+                cout << endl;
+                continue;
+            }
+            cout << phoneBook[i]->name << " " << phoneBook[i]->phoneNumber;
+            cout << endl;
+        }
+        cout << endl << "--------------- \n";
+    }
+};
+
+int main()
+{
+    HashMap<10> thobook;
+    thobook.Insert("Tho", "090312231");
+    thobook.Insert("thanh ga", "021321312");
+    thobook.Insert("Quan gar", "21321321");
+    thobook.Insert("Somi", "21321321");
+    thobook.Insert("Nam", "69696321");
+    thobook.Insert("Nghi", "1234521");
+
+    thobook.Search("thanh ga");
+    thobook.Search("th");
+    // thobook.Display();
+    // thobook.DeleteKey("Somi");
+    // thobook.Display();
+    // cout << " test 1 " << endl;
+
+    cout << "finish";
+    return 0;
+}
