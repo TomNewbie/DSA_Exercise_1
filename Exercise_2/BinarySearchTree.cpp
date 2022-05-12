8b4142b7e17033b95af9ff16f1bbc4ec3bd53d40
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-05-12 14:26:03 +0700

    resolve conflict

--- Exercise_2/BinarySearchTree.cpp
+++ Exercise_2/BinarySearchTree.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 #include <queue>
 using namespace std;
 
@@ -8,38 +8,33 @@ struct Node {
     int data;
     Node *left, *right;
 };
-Node* createNode (int input){
-    Node* temp = new Node;
+Node *createNode(int input)
+{
+    Node *temp = new Node;
     temp->data = input;
     temp->left = temp->right = NULL;
     return temp;
 }
 void inorder(Node *root)
 {
-	if (root != NULL)
-	{
-		inorder(root->left);
-		cout << root->data<<" ";
-		inorder(root->right);
-	}
+    if (root != NULL) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
 }
-struct Node* insert (struct Node* node, int key){
+struct Node *insert(struct Node *node, int key)
+{
     if (node == NULL)
-    return createNode (key);
-<<<<<<< Updated upstream
-    
-    if (key <= node->data)
-    node->left = insert (node->left, key);
-    else node->right = insert (node->right, key);
-=======
+        return createNode(key);
 
-    if (key < node->data)
-    node -> left = insert (node -> left, key);
-    else node -> right = insert (node -> right, key);
->>>>>>> Stashed changes
+    if (key <= node->data)
+        node->left = insert(node->left, key);
+    else
+        node->right = insert(node->right, key);
     return node;
 }
-bool ifNodeExists(struct Node* node, int key)
+bool ifNodeExists(struct Node *node, int key)
 {
     if (node == NULL)
         return false;
@@ -50,7 +45,8 @@ bool ifNodeExists(struct Node* node, int key)
     /* then recur on left subtree */
     bool res1 = ifNodeExists(node->left, key);
     // node found, no need to look further
-    if(res1) return true;
+    if (res1)
+        return true;
 
     /* node is not found in left,
     so recur on right subtree */
@@ -58,112 +54,108 @@ bool ifNodeExists(struct Node* node, int key)
 
     return res2;
 }
-int* create_array(int length)
+int *create_array(int length)
 {
-    int b = 500, a= 1;
-    srand((unsigned) time(0));
-    int* data = (int*) malloc(sizeof(int)*length);
-    for (int i=0;i<length;++i)
-    {
-        data[i]=rand()%(b-a+1)+a;
+    int b = 500, a = 1;
+    srand((unsigned)time(0));
+    int *data = (int *)malloc(sizeof(int) * length);
+    for (int i = 0; i < length; ++i) {
+        data[i] = rand() % (b - a + 1) + a;
     }
     return data;
 }
-int FindMin(Node* root)
+int FindMin(Node *root)
 {
-    while(root->left!=NULL)
-        root=root->left; //go to the left until minimum value
+    while (root->left != NULL)
+        root = root->left; // go to the left until minimum value
     return root->data;
 }
-Node* Delete(struct Node *root, int data)
+Node *Delete(struct Node *root, int data)
 {
-    if(root==NULL) return root; //empty tree
-    else if(data < root->data)
-        root->left=Delete(root->left,data);
-    else if(data > root->data)
-        root->right=Delete(root->right,data);
-    else
-    {
-        //C1: No child
-        if(root->left==NULL&&root->right==NULL)
-            {
+    if (root == NULL)
+        return root; // empty tree
+    else if (data < root->data)
+        root->left = Delete(root->left, data);
+    else if (data > root->data)
+        root->right = Delete(root->right, data);
+    else {
+        // C1: No child
+        if (root->left == NULL && root->right == NULL) {
             delete root;
-            root=NULL;
+            root = NULL;
             return root;
-            }
+        }
 
-        //C2: 1 child
-        else if(root->left==NULL)
-        {
-            struct Node *temp=root;
-            root=root->right; //make right child new root of subtree
+        // C2: 1 child
+        else if (root->left == NULL) {
+            struct Node *temp = root;
+            root = root->right; // make right child new root of subtree
             delete temp;
         }
-        else if(root->right==NULL)
-        {
-            struct Node *temp=root;
-            root=root->left; //make left child new root of subtree
+        else if (root->right == NULL) {
+            struct Node *temp = root;
+            root = root->left; // make left child new root of subtree
             delete temp;
         }
 
-        //C3: 2 children
-        else
-        {
-            int temp= FindMin(root->right); //start from right child of root
-            root->data=temp;
-            root->right=Delete(root->right,temp);
+        // C3: 2 children
+        else {
+            int temp = FindMin(root->right); // start from right child of root
+            root->data = temp;
+            root->right = Delete(root->right, temp);
         }
     }
     return root;
 }
 
-void breadth_first_traversal(Node* root){ 
-    //print all the element on each level
-    if(root == NULL) cout << "This tree is empty stupid";
-    else{
-        queue<Node*> Q; //why new not work in here ?>??
+void breadth_first_traversal(Node *root)
+{
+    // print all the element on each level
+    if (root == NULL)
+        cout << "This tree is empty stupid";
+    else {
+        queue<Node *> Q; // why new not work in here ?>??
         Q.push(root);
-        while(!Q.empty()){
-            Node* current = Q.front();
+        while (!Q.empty()) {
+            Node *current = Q.front();
             cout << current->data << "  ";
-            if(current->left != NULL) Q.push(current->left);
-            if(current->right != NULL) Q.push(current->right);
+            if (current->left != NULL)
+                Q.push(current->left);
+            if (current->right != NULL)
+                Q.push(current->right);
             Q.pop();
         }
     }
-    cout <<endl;
+    cout << endl;
 }
-Node* BSTforming(Node *root, int* array, int length)
+Node *BSTforming(Node *root, int *array, int length)
 {
-	for (int i=0;i<length;i++)
-	{
-	    root = insert(root,array[i]);
-	}
-	return root;
+    for (int i = 0; i < length; i++) {
+        root = insert(root, array[i]);
+    }
+    return root;
 }
-int main() {
-    Node* root = NULL;
+int main()
+{
+    Node *root = NULL;
 
-	//Create an array of 25 elements
-	int array_length = 25;
-	int* random_array = create_array(array_length);
-    
-    root = BSTforming(root,random_array,array_length);
+    // Create an array of 25 elements
+    int array_length = 25;
+    int *random_array = create_array(array_length);
 
-	//Forming and performing preorder traversal of a 25-node tree
+    root = BSTforming(root, random_array, array_length);
+
+    // Forming and performing preorder traversal of a 25-node tree
     cout << "Inorder traversal of the given tree:" << endl;
-	inorder(root);
-    cout <<endl;
+    inorder(root);
+    cout << endl;
     // Delete a value in root;
     root = Delete(root, root->data);
-<<<<<<< Updated upstream
     root = Delete(root, root->data);
     root = Delete(root, root->data);
     root = Delete(root, root->data);
     root = Delete(root, root->data);
     root = Delete(root, root->data);
-=======
->>>>>>> Stashed changes
     cout << "Inorder traversal of the given tree:" << endl;
     inorder(root);
     return 0;
